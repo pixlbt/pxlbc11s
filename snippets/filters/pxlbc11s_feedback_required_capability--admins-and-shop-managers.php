abf3c66fbe054c1080654cf6f21508a2e993be9d
pixlbt <admin@pixelbot.online>
2025-09-14 22:15:08 +0200

    Update pxlbc11s_feedback_required_capability--admins-and-shop-managers.php

--- snippets/filters/pxlbc11s_feedback_required_capability--admins-and-shop-managers.php
+++ snippets/filters/pxlbc11s_feedback_required_capability--admins-and-shop-managers.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * pxlbc11s_feedback_required_capability filter
+ * lighten or customize the capability required to submit feedback
+ */
+
 // allow administrators and shop managers
 add_filter( 'pxlbc11s_feedback_required_capability', function ( $defaultCapability ) {
 	return 'manage_woocommerce';
