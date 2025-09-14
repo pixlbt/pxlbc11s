5acc6d74f262e7fa6f2f846764e99a35f4259f6c
pixlbt <admin@pixelbot.online>
2025-09-14 22:17:14 +0200

    Create pxlbc11s_feedback_required_capability--shop-support.php

---
+++ snippets/filters/pxlbc11s_feedback_required_capability--shop-support.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * pxlbc11s_feedback_required_capability filter
+ * lighten or customize the capability required to submit feedback
+ */
+
+// allow shop support staff who manage orders or products
+add_filter( 'pxlbc11s_feedback_required_capability', function ( $defaultCapability ) {
+	return [ 'edit_shop_orders', 'edit_products' ];
+}, 10, 1 );
